b51885cbf11474a3ca847bfb23e0973edfc3c613
Caio Vieira <caioviier@gmail.com>
2024-07-21 04:22:21 -0300

    include first method

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,16 @@ export default {
       link: `https://google.com`
     };
   },
+  methods: {
+    toggleStatus(){
+      if(this.status == `active`)
+        this.status = `pending`
+      else if(this.status == `pending`)
+        this.status = `inactive`
+      else
+        this.status = `active`
+    }
+  }
 };
 </script>
 
@@ -19,8 +29,10 @@ export default {
 
   <h3>Tasks:</h3>
   <ul>
-    <li v-for="task in tasks" :key="task">{{ task }} /li></li>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
   </ul>
 
-  <a v-bind:href="link">Click for Google</a>
+  <a :href="link">Click for Google</a>
+
+  <button v-on:click="toggleStatus">Change Status</button>
 </template>
